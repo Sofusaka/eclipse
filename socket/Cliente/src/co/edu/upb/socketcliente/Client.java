11ec6ed6553682a2d66caaa58ce6eaf5a5d4d5b2
Sofía princess <sofusaka@gmail.com>
2025-01-29 00:54:01 -0500

    mensajes
    ya manda mensajes jaja

--- socket/Cliente/src/co/edu/upb/socketcliente/Client.java
+++ socket/Cliente/src/co/edu/upb/socketcliente/Client.java
@@ -1,15 +1,43 @@
 package co.edu.upb.socketcliente;
+import java.util.Scanner;
 
 public class Client {
-	
+	String opcion;
 	JSocketClient sk;
+	 public Scanner scanner;
 	
 	public Client(String address, int port) {
-		sk = new JSocketClient(address, port);		
+		sk = new JSocketClient(address, port);	
+		scanner = new Scanner(System.in);
 	}
 	
 	public void opetation(String data) {
 		this.sk.request(data);
 	}
+	
+	public void message() {
+		
+		
+		 while (true) {
+	            System.out.println("\n[Cliente]: ESCOGE UNA OPCIÓN \n1. Enviar mensaje \n2. Cerrar conexión");
+	            System.out.print("Ingrese opción: ");
+	            opcion = scanner.nextLine(); 
 
-}
+	            if (opcion.equals("1")) {
+	                System.out.print("Ingrese el mensaje a enviar: ");
+	                String mensaje = scanner.nextLine();
+	                sk.request(mensaje); 
+	            } else if (opcion.equals("2")) {
+	                System.out.println("\n[Cliente]: Cerrando conexión...");
+	                sk.closeService(); 
+	                break;
+	            } else {
+	                System.out.println("[Cliente]: Opción no válida. Intente de nuevo.");
+	            }
+	        }
+	        scanner.close(); 
+	    }
+		
+	}
+
+
